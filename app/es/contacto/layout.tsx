a97a879264fdcb90f2e565b7e03984eb7f7a9573
Miquel Solà Espallargas <miquelsolaa@gmail.com>
2025-03-01 23:52:33 +0100

    corrected titles

--- app/es/contacto/layout.tsx
+++ app/es/contacto/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Contacto",
+  title: "Stop Bullying - Contacto",
   description: "Ponte en contacto con nosotros para cualquier consulta o para reservar tu primera sesión.",
 }
 
