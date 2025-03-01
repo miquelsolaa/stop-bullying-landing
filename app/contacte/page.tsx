c24c3c615f7d09c054f77db09f06c25f23ddf867
Miquel Solà Espallargas <miquelsolaa@gmail.com>
2025-03-01 20:01:43 +0100

    Form update

--- app/contacte/page.tsx
+++ app/contacte/page.tsx
@@ -63,7 +63,7 @@ export default function ContactePage() {
                   </p>
                 </div>
 
-                <form className="space-y-6" netlify>
+                <form name="contact" className="space-y-6" netlify>
                   <div className="space-y-2">
                     <label htmlFor="name" className="text-sm font-medium">
                       Nom
